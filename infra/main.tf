bd3ae4de49105ea9d8355d680962db4d4a6bde21
vladimir.beglyak <vladimir.beglyak@solva.kz>
2025-11-23 22:13:49 +0300

    initial k8s release

--- infra/main.tf
+++ infra/main.tf
@@ -25,3 +25,21 @@ module "aws-network" {
   public_subnet_a_cidr  = "10.10.128.0/18"
   public_subnet_b_cidr  = "10.10.192.0/18"
 }
+
+module "aws-eks" {
+  source = "github.com/VladimirBeglyak/module-aws-kubernetes"
+
+  mc_namespace       = "microsrvices"
+  env_name           = local.env_name
+  aws_region         = local.aws_region
+  cluster_name       = local.k8s_cluster_name
+  vpc_id             = module.aws-network.vpc_id
+  cluster_subnet_ids = module.aws-network.subnet_ids
+
+  nodegroup_subnet_ids     = module.aws-network.private_subnet_ids
+  nodegroup_disk_size      = "20"
+  nodegroup_instance_types = ["t3.medium"]
+  nodegroup_desired_size   = 1
+  nodegroup_min_size       = 1
+  nodegroup_max_size       = 3
+}
